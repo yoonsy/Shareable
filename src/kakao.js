b447190dc7b0820d0e92a223197060e587be4d7a
yoonsy <nscorpio16@gmail.com>
2014-10-11 01:16:02 +0900

    카카오스토리 링크 받을수 있게

--- src/kakao.js
+++ src/kakao.js
@@ -14,25 +14,16 @@
   }
 
 
-  Shareable.prototype.kakaostory = function(shareUrl){
+  Shareable.prototype.kakaostory = function(shareUrl, appOptions){
 
     var shareUrl = shareUrl || location.href;
 
     if( navigator.userAgent.toLowerCase().match(/android|iphone/) ){
-      kakao.link("story").send({
-        post : "http://m.media.daum.net/entertain/enews/view?newsid=20120927110708426",
-        appid : "m.media.daum.net",
-        appver : "1.0",
-        appname : "미디어디음",
-        urlinfo : JSON.stringify({
-          title:"(광해) 실제 역사적 진실은?",
-          desc:"(광해 왕이 된 남자)의 역사성 부족을 논하다.",
-          imageurl:[
-            "http://m1.daumcdn.net/photo-media/201209/27/ohmynews/R_430x0_20120927141307222.jpg"
-          ],
-          type:"article"
-        })
-      });
+
+      // appOptions
+      // https://github.com/kakao/kakaolink-web
+
+      kakao.link("story").send(appOptions);
 
     } else {
 
